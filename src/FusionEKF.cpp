cb888bf88d73d0cc53f92e88131a878173335ef1
Sijmen <sijmen.vanderwillik@jstack.eu>
2018-09-24 17:34:35 +0200

    fix typo, add measurement update

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -72,7 +72,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
             */
             double rho = measurement_pack.raw_measurements_(0);
             double phi = measurement_pack.raw_measurements_(1);
-            double rhoDot = measurement_pack.raw_measurements_(2);
+            double rhodot = measurement_pack.raw_measurements_(2);
 
             double px = rho * cos(phi);
             double py = rho * sin(phi);
@@ -87,6 +87,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
             double py = measurement_pack.raw_measurements_(1);
             ekf_.x_ << px, py, 0.0, 0.0;
         }
+
+        previous_timestamp_ = measurement_pack.timestamp_;
 
         // done initializing, no need to predict or update
         is_initialized_ = true;
@@ -105,6 +107,24 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
        * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
      */
 
+    // add dt to state transition matrix F
+    ekf_.F_(0, 2) = dt;
+    ekf_.F_(1, 3) = dt;
+
+    // build Q matrix
+    double dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000; // dt in seconds
+    double dt2 = dt * dt;
+    double dt3 = dt2 * dt;
+    double dt4 = dt3 * dt;
+    double noise_ax = 9;
+    double noise_ay = 9;
+
+    ekf_.Q_ =  MatrixXd(4, 4);
+    ekf_.Q_ <<  dt4 / 4 * noise_ax, 0, dt3 / 2 * noise_ax, 0,
+                0, dt4 / 4 * noise_ay, 0, dt3 / 2 * noise_ay,
+                dt3 / 2 * noise_ax, 0, dt2 * noise_ax, 0
+                0, dt3 / 2 * noise_ay, 0, dt2 * noise_ay;
+
     ekf_.Predict();
 
     /*****************************************************************************
@@ -119,8 +139,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
         // Radar updates
+        Hj_ = Tools::CalculateJacobian(ekf_.x_);
+        ekf_.H_ = Hj_;
+        ekf_.R_ = R_radar_;
+        ekf_.Update(measurement_pack.raw_measurements_);
     } else {
         // Laser updates
+        ekf_.H_ = H_laser_;
+        ekf_.R_ = R_laser_;
+        ekf_.Update(measurement_pack.raw_measurements_);
     }
 
     // print the output
